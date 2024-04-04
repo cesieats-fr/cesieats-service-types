6f60eb2eaa3666a0f8349ccca102d2ec4bc1d4c5
Keanu <keanuharrell@hotmail.fr>
2024-04-04 11:38:15 +0200

    add mongoose schema and model to account

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,5 +1,15 @@
-export interface Account {
+import { Schema, model } from "mongoose";
+
+export interface IAccount {
     id: number;
     email: string;
     password: string;
-}
+};
+
+export const accountSchema = new Schema<IAccount>({
+    id: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+export const Account = model<IAccount>('Account', accountSchema);
