8a4242c7a398dc3b3ceef280e9d05efe0dfd9ad0
Keanu <keanuharrell@hotmail.fr>
2024-04-04 11:25:46 +0200

    initial push

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const account = import('./account');
+const delivery = import('./delivery');
+const file = import('./file');
+const identity = import('./identity');
+
+export default {
+    account, 
+    delivery,
+    file,
+    identity,
+}
